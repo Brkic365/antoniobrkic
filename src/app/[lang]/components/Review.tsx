6f9825adb09ea2fda467b6a8b2a51e5286bb7997
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2024-01-31 14:00:12 +0100

    Added animations

--- src/app/[lang]/components/Review.tsx
+++ src/app/[lang]/components/Review.tsx
@@ -3,22 +3,81 @@ import styles from "@/styles/Review.module.scss";
 
 import { ReviewType } from "@/types/review";
 
+import { motion } from "framer-motion";
+
 function Review({ review, lang }: { review: ReviewType; lang: string }) {
   return (
     <section className={styles.review}>
       <section className={styles.top}>
-        <img
+        <motion.img
           src={`/images/pfp/${review.pfp}.webp`}
           alt={`${review.name}'s Profile Picture`}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          variants={{
+            visible: {
+              opacity: 1,
+            },
+            hidden: {
+              opacity: 0,
+            },
+          }}
         />
         <div className={styles.text}>
-          <h4>{review.name}</h4>
-          <p className={styles.position}>{review.position}</p>
+          <motion.h4
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+              hidden: {
+                opacity: 0,
+              },
+            }}
+          >
+            {review.name}
+          </motion.h4>
+          <motion.p
+            className={styles.position}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+              hidden: {
+                opacity: 0,
+              },
+            }}
+          >
+            {review.position}
+          </motion.p>
         </div>
       </section>
-      <p className={styles.reviewText}>
+      <motion.p
+        className={styles.reviewText}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.55 }}
+        variants={{
+          visible: {
+            opacity: 1,
+          },
+          hidden: {
+            opacity: 0,
+          },
+        }}
+      >
         {review.reviews[lang as keyof typeof review.reviews]}
-      </p>
+      </motion.p>
     </section>
   );
 }
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -20,6 +20,8 @@ import REVIEWS from "@/data/reviews.json";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
+import { motion } from "framer-motion";
+
 const OPTIONS: EmblaOptionsType = {
   containScroll: "trimSnaps",
   loop: true,
@@ -136,45 +138,276 @@ export default function Home({
       <section className={styles.hero}>
         <section className={styles.text}>
           <section className={styles.name}>
-            <img
+            <motion.img
               src={`/images/stickers/available_now.webp`}
               alt="Available now sticker"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              variants={{
+                visible: {
+                  transform: "scale(1) rotate(8.744deg)",
+                  filter: "blur(0px)",
+                  opacity: 1,
+                },
+                hidden: {
+                  transform: "scale(1.5) rotate(0deg)",
+                  filter: "blur(4px)",
+                  opacity: 0,
+                },
+              }}
             />
-            <h1 className={styles.firstName}>ANTONIO</h1>
+            <motion.h1
+              className={styles.firstName}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              variants={{
+                visible: {
+                  transform: "scale(1)",
+                  filter: "blur(0px)",
+                  opacity: 1,
+                },
+                hidden: {
+                  transform: "scale(1.5)",
+                  filter: "blur(4px)",
+                  opacity: 0,
+                },
+              }}
+            >
+              ANTONIO
+            </motion.h1>
             <div className={styles.lastName}>
-              <h1 className={styles.lastName}>BRKIC</h1>
+              <motion.p
+                className={styles.innerDesc}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                variants={{
+                  visible: { transform: "translateX(0)", opacity: 1 },
+                  hidden: { transform: "translateX(-100%)", opacity: 0 },
+                }}
+              >
+                {dict.description}
+              </motion.p>
+              <motion.h1
+                className={styles.lastName}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+                variants={{
+                  visible: {
+                    transform: "scale(1)",
+                    filter: "blur(0px)",
+                    opacity: 1,
+                  },
+                  hidden: {
+                    transform: "scale(1.5)",
+                    filter: "blur(4px)",
+                    opacity: 0,
+                  },
+                }}
+              >
+                BRKIC
+              </motion.h1>
             </div>
           </section>
-          <p className={styles.outsideDesc}>{dict.description}</p>
-          <div className={styles.button}>
+          <motion.p
+            className={styles.outsideDesc}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            variants={{
+              visible: { transform: "translateX(0)", opacity: 1 },
+              hidden: { transform: "translateX(-100%)", opacity: 0 },
+            }}
+          >
+            {dict.description}
+          </motion.p>
+          <motion.div
+            className={styles.button}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            variants={{
+              visible: { transform: "translateX(0)", opacity: 1 },
+              hidden: { transform: "translateX(-100%)", opacity: 0 },
+            }}
+          >
             <p>{dict.hero_cta}</p> <HiArrowRight />
-          </div>
+          </motion.div>
         </section>
 
-        <img
+        <motion.img
           src={`/images/${dict.hero_image}`}
           alt="Yugioh Graphic"
           className={styles.heroImage}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          variants={{
+            visible: {
+              transform: "scale(1) rotate(0deg)",
+              filter: "blur(0px)",
+              opacity: 1,
+            },
+            hidden: {
+              transform: "scale(1.5) rotate(5deg)",
+              filter: "blur(4px)",
+              opacity: 0,
+            },
+          }}
         />
       </section>
 
       <section className={styles.about} id="about">
         <div className={styles.title}>
-          <h1>{dict.about_title}</h1>
-          <h2>{dict.about_title}</h2>
+          <motion.h1
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.75, delay: 0.2 }}
+            variants={{
+              visible: {
+                transform: "scale(1)",
+                filter: "blur(0px)",
+                opacity: 1,
+              },
+              hidden: {
+                transform: "scale(1.5)",
+                filter: "blur(4px)",
+                opacity: 0,
+              },
+            }}
+          >
+            {dict.about_title}
+          </motion.h1>
+          <motion.h2
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+              hidden: {
+                opacity: 0,
+              },
+            }}
+          >
+            {dict.about_title}
+          </motion.h2>
         </div>
 
         <section className={styles.content}>
-          <p className={styles.firstParagraph}>{dict.about_descriptions[0]}</p>
-          <img src={`/images/about.webp`} alt="About Me Graphic" />
-          <p className={styles.secondParagraph}>{dict.about_descriptions[1]}</p>
+          <motion.p
+            className={styles.firstParagraph}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            variants={{
+              visible: {
+                transform: "scale(1) rotate(0deg)",
+                filter: "blur(0px)",
+                opacity: 1,
+              },
+              hidden: {
+                transform: "scale(1.5) rotate(3deg)",
+                filter: "blur(4px)",
+                opacity: 0,
+              },
+            }}
+          >
+            {dict.about_descriptions[0]}
+          </motion.p>
+          <motion.img
+            src={`/images/about.webp`}
+            alt="About Me Graphic"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            variants={{
+              visible: {
+                transform: "scale(1) rotate(0deg)",
+                filter: "blur(0px)",
+                opacity: 1,
+              },
+              hidden: {
+                transform: "scale(1.5) rotate(3deg)",
+                filter: "blur(4px)",
+                opacity: 0,
+              },
+            }}
+          />
+          <motion.p
+            className={styles.secondParagraph}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            variants={{
+              visible: {
+                transform: "scale(1) rotate(0deg)",
+                filter: "blur(0px)",
+                opacity: 1,
+              },
+              hidden: {
+                transform: "scale(1.5) rotate(3deg)",
+                filter: "blur(4px)",
+                opacity: 0,
+              },
+            }}
+          >
+            {dict.about_descriptions[1]}
+          </motion.p>
         </section>
       </section>
 
-      <div className={styles.seperator} />
+      <motion.div
+        className={styles.seperator}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        variants={{
+          visible: {
+            opacity: 1,
+            width: "80%",
+          },
+          hidden: {
+            opacity: 0,
+            width: 0,
+          },
+        }}
+      />
 
       <section className={styles.myWork} id="my-work">
-        <h2>{dict.my_work_title}</h2>
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          variants={{
+            visible: {
+              opacity: 1,
+            },
+            hidden: {
+              opacity: 0,
+            },
+          }}
+        >
+          {dict.my_work_title}
+        </motion.h2>
         <Carousel
           projects={PROJECTS}
           options={OPTIONS}
@@ -184,7 +417,22 @@ export default function Home({
       </section>
 
       <section className={styles.reviews} id="reviews">
-        <h2>{dict.reviews_title}</h2>
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          variants={{
+            visible: {
+              opacity: 1,
+            },
+            hidden: {
+              opacity: 0,
+            },
+          }}
+        >
+          {dict.reviews_title}
+        </motion.h2>
         <section className={styles.grid}>
           {REVIEWS.map((review, i) => (
             <Review review={review} key={i} lang={localeShort} />
@@ -194,59 +442,192 @@ export default function Home({
 
       <section className={styles.contact} id="contact">
         <section className={styles.title}>
-          <img
+          <motion.img
             src={`/images/stickers/antonio_brkic.webp`}
             alt="Antonio Brkic sticker"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            variants={{
+              visible: {
+                transform: "scale(1) rotate(8.744deg)",
+                filter: "blur(0px)",
+                opacity: 1,
+              },
+              hidden: {
+                transform: "scale(1.5) rotate(0deg)",
+                filter: "blur(4px)",
+                opacity: 0,
+              },
+            }}
           />
-          <h2>{dict.contact_title}</h2>
+          <motion.h2
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+              hidden: {
+                opacity: 0,
+              },
+            }}
+          >
+            {dict.contact_title}
+          </motion.h2>
           <div className={styles.contactInfo}>
-            <p>{dict.inputs.email}: brkicweb@gmail.com</p>
-            <p>{dict.inputs.phone}: +385 95 760 2280</p>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
+            >
+              {dict.inputs.email}: brkicweb@gmail.com
+            </motion.p>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
+            >
+              {dict.inputs.phone}: +385 95 760 2280
+            </motion.p>
           </div>
         </section>
 
         <form>
           <div className={styles.inputRow}>
-            <input
+            <motion.input
               type="text"
               placeholder={`${dict.inputs.name.toUpperCase()}*`}
               id="name"
               ref={nameRef}
               onChange={updateCanSubmit}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
             />
-            <input
+            <motion.input
               type="email"
               placeholder={`${dict.inputs.email.toUpperCase()}*`}
               ref={emailRef}
               onChange={updateCanSubmit}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.6 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
             />
           </div>
           <div className={styles.inputRow}>
-            <input
+            <motion.input
               type="tel"
               placeholder={`${dict.inputs.phone.toUpperCase()}*`}
               id="tel"
               ref={phoneRef}
               onChange={updateCanSubmit}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.7 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
             />
-            <input
+            <motion.input
               type="number"
               placeholder={`${dict.inputs.budget.toUpperCase()}*`}
               ref={budgetRef}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.8 }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                },
+                hidden: {
+                  opacity: 0,
+                },
+              }}
             />
           </div>
-          <textarea
+          <motion.textarea
             placeholder={`${dict.inputs.message.toUpperCase()}*`}
             rows={10}
             ref={messageRef}
             onChange={updateCanSubmit}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.9 }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+              hidden: {
+                opacity: 0,
+              },
+            }}
           />
-          <div
+          <motion.div
             className={canSubmit ? styles.button : styles.inactiveButton}
             onClick={submitForm}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            variants={{
+              visible: {
+                transform: "scale(1)",
+              },
+              hidden: {
+                transform: "scale(0)",
+              },
+            }}
           >
             <p>{dict.inputs.submit}</p> <HiArrowRight />
-          </div>
+          </motion.div>
         </form>
       </section>
     </main>
